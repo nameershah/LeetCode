d4ee1c3a8ad63ae2418a8edd728b40b0e8b3272c
Muhammad Nameer Shah <190166225+nameershah@users.noreply.github.com>
2026-09-07 17:03:15 +0500

    Time: 186 ms (57.08%), Space: 19.8 MB (63.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,13 +3,13 @@ class Solution:
         char_map = {}  # Tracks { character: most_recent_index }
         left = 0
         res = 0
-        
+
         for right, char in enumerate(s):
             # If the character is in our current window, jump 'left' past it
             if char in char_map and char_map[char] >= left:
                 left = char_map[char] + 1
-            
+
             char_map[char] = right
             res = max(res, right - left + 1)
-            
+
         return res
