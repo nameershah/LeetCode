12c49dbaf4791c1a02bbb3690a2137723e2b616c
Muhammad Nameer Shah <190166225+nameershah@users.noreply.github.com>
2026-09-08 01:02:14 +0500

    Time: 47 ms (35.64%), Space: 24.5 MB (59.46%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -6,9 +6,12 @@ class RecentCounter:
         self.q = deque()
 
     def ping(self, t):
+        # 1. Add the new request
         self.q.append(t)
 
+        # 2. Remove requests that are too old
         while self.q[0] < t - 3000:
             self.q.popleft()
 
+        # 3. Return the number of recent requests
         return len(self.q)
